59d28c60150f24efd2df8c534659481c9f15312f
LakshmiMekala <lmekala@tibco.com>
2019-08-27 09:36:22 +0530

    remove unwanted code from bm tests

--- ruleapi/tests/rules_1_test.go
+++ ruleapi/tests/rules_1_test.go
@@ -72,6 +72,8 @@ func BenchmarkTestApiOne(b *testing.B) {
 	rs, _ := createRuleSession()
 
 	actionMap := make(map[string]string)
+
+	// rule 1
 	r1 := ruleapi.NewRule("R1")
 	r1.AddCondition("C1", []string{"t1"}, checkC1, nil)
 	r1.SetAction(actionA1)
@@ -80,9 +82,7 @@ func BenchmarkTestApiOne(b *testing.B) {
 	rs.AddRule(r1)
 
 	rs.Start(nil)
-	fmt.Println("Alert message 1")
 	for n := 0; n < b.N; n++ {
-		//// rule 1
 		s1 := rand.NewSource(time.Now().UnixNano())
 		r1 := rand.New(s1)
 		t1, _ := model.NewTupleWithKeyValues("t1", "Tom"+strconv.Itoa(r1.Intn(1000000000000)))
@@ -93,15 +93,15 @@ func BenchmarkTestApiOne(b *testing.B) {
 			b.Error(err)
 			b.Fail()
 		}
-		// time.Sleep(1000 * time.Nanosecond)
 	}
 	rs.Unregister()
 }
 
 func BenchmarkTestApiTwo(b *testing.B) {
 	rs, _ := createRuleSession()
-	fmt.Println("Alert message 2")
 	actionMap := make(map[string]string)
+
+	// rule 1
 	r1 := ruleapi.NewRule("R1")
 	r1.AddCondition("C1", []string{"t1"}, checkC1, nil)
 	r1.SetAction(actionA1)
@@ -109,6 +109,7 @@ func BenchmarkTestApiTwo(b *testing.B) {
 	r1.SetContext(actionMap)
 	rs.AddRule(r1)
 
+	// rule 2
 	r2 := ruleapi.NewRule("R2")
 	r2.AddCondition("C2", []string{"t1"}, checkC2, nil)
 	r2.SetAction(actionA2)
@@ -129,7 +130,7 @@ func BenchmarkTestApiTwo(b *testing.B) {
 	rs.Start(nil)
 
 	for n := 0; n < b.N; n++ {
-		//// rule 1
+
 		s1 := rand.NewSource(time.Now().UnixNano())
 		r1 := rand.New(s1)
 		t1, _ := model.NewTupleWithKeyValues("t1", "Tom"+strconv.Itoa(r1.Intn(1000000000000)))
@@ -140,35 +141,29 @@ func BenchmarkTestApiTwo(b *testing.B) {
 			b.Error(err)
 			b.Fail()
 		}
-		// time.Sleep(1000 * time.Nanosecond)
 	}
 	rs.Unregister()
 }
 
 func checkC1(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
-	fmt.Println("In Condition C1")
 	return true
 }
 
 func actionA1(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
-	fmt.Println("In Action A1 Start")
 
 	// change t1 field
 	t1 := tuples["t1"].(model.MutableTuple)
 	t1.SetString(ctx, "p3", "somethingnew")
 
-	fmt.Println("In Action A1 End")
 	firedMap := ruleCtx.(map[string]string)
 	firedMap["A1"] = "Fired"
 }
 
 func checkC2(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
-	fmt.Println("In Condition C2")
 	return true
 }
 
 func actionA2(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
-	fmt.Println("In Action A2 Start")
 	t1 := tuples["t1"]
 	val, _ := t1.GetString("p3")
 	fmt.Println("In Action A2 End ", val)
@@ -177,12 +172,10 @@ func actionA2(ctx context.Context, rs model.RuleSession, ruleName string, tuples
 }
 
 func checkC3(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
-	fmt.Println("In Condition C3")
 	return true
 }
 
 func actionA3(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
-	fmt.Println("In Action A3 Start")
 	t1 := tuples["t1"]
 	val, _ := t1.GetString("p3")
 	fmt.Println("In Action A3 End ", val)
