5afd4cc8c3194175da2dd56184549b8f379a0da2
LakshmiMekala <lmekala@tibco.com>
2019-09-05 16:19:51 +0530

    Add bm test for store fix branch

--- ruleapi/tests/rules_1_test.go
+++ ruleapi/tests/rules_1_test.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
-	"strconv"
 	"testing"
-	"time"
 
 	"github.com/project-flogo/rules/common/model"
 	"github.com/project-flogo/rules/ruleapi"
@@ -68,6 +66,51 @@ func Test_One(t *testing.T) {
 	}
 }
 
+func checkC1(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
+	// fmt.Println("In Condition C1")
+	return true
+}
+
+func actionA1(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
+	// fmt.Println("In Action A1 Start")
+
+	// change t1 field
+	t1 := tuples["t1"].(model.MutableTuple)
+	t1.SetString(ctx, "p3", "somethingnew")
+
+	// fmt.Println("In Action A1 End")
+	firedMap := ruleCtx.(map[string]string)
+	firedMap["A1"] = "Fired"
+}
+
+func checkC2(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
+	fmt.Println("In Condition C2")
+	return true
+}
+
+func actionA2(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
+	fmt.Println("In Action A2 Start")
+	t1 := tuples["t1"]
+	val, _ := t1.GetString("p3")
+	fmt.Println("In Action A2 End ", val)
+	firedMap := ruleCtx.(map[string]string)
+	firedMap["A2"] = "Fired"
+}
+
+func checkC3(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
+	fmt.Println("In Condition C3")
+	return true
+}
+
+func actionA3(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
+	fmt.Println("In Action A3 Start")
+	t1 := tuples["t1"]
+	val, _ := t1.GetString("p3")
+	fmt.Println("In Action A3 End ", val)
+	firedMap := ruleCtx.(map[string]string)
+	firedMap["A3"] = "Fired"
+}
+
 func BenchmarkTestApiOne(b *testing.B) {
 	rs, _ := createRuleSession()
 
@@ -83,13 +126,15 @@ func BenchmarkTestApiOne(b *testing.B) {
 
 	rs.Start(nil)
 	for n := 0; n < b.N; n++ {
-		s1 := rand.NewSource(time.Now().UnixNano())
-		r1 := rand.New(s1)
-		t1, _ := model.NewTupleWithKeyValues("t1", "Tom"+strconv.Itoa(r1.Intn(1000000000000)))
+		// s1 := rand.NewSource(time.Now().UnixNano())
+		// r1 := rand.New(s1)
+
+		// t1, _ := model.NewTupleWithKeyValues("t1", "Tom"+strconv.Itoa(r1.Intn(1000000000000000000)))
+		t1, _ := model.NewTupleWithKeyValues("t1", "Tom"+randomString(randomBytes(16)))
 		t1.SetString(nil, "p3", "test")
 		err := rs.Assert(nil, t1)
 		if err != nil {
-			fmt.Println("Tom" + strconv.Itoa(r1.Intn(1000000000000)))
+			fmt.Println("Tom")
 			b.Error(err)
 			b.Fail()
 		}
@@ -97,88 +142,22 @@ func BenchmarkTestApiOne(b *testing.B) {
 	rs.Unregister()
 }
 
-func BenchmarkTestApiTwo(b *testing.B) {
-	rs, _ := createRuleSession()
-	actionMap := make(map[string]string)
+const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/"
 
-	// rule 1
-	r1 := ruleapi.NewRule("R1")
-	r1.AddCondition("C1", []string{"t1"}, checkC1, nil)
-	r1.SetAction(actionA1)
-	r1.SetPriority(1)
-	r1.SetContext(actionMap)
-	rs.AddRule(r1)
-
-	// rule 2
-	r2 := ruleapi.NewRule("R2")
-	r2.AddCondition("C2", []string{"t1"}, checkC2, nil)
-	r2.SetAction(actionA2)
-	r2.SetPriority(2)
-	r2.SetContext(actionMap)
-
-	rs.AddRule(r2)
-
-	// rule 3
-	r3 := ruleapi.NewRule("R3")
-	r3.AddCondition("C3", []string{"t1"}, checkC3, nil)
-	r3.SetAction(actionA3)
-	r3.SetPriority(3)
-	r3.SetContext(actionMap)
-
-	rs.AddRule(r3)
-
-	rs.Start(nil)
-
-	for n := 0; n < b.N; n++ {
-
-		s1 := rand.NewSource(time.Now().UnixNano())
-		r1 := rand.New(s1)
-		t1, _ := model.NewTupleWithKeyValues("t1", "Tom"+strconv.Itoa(r1.Intn(1000000000000)))
-		t1.SetString(nil, "p3", "test")
-		err := rs.Assert(nil, t1)
-		if err != nil {
-			fmt.Println("Tom" + strconv.Itoa(r1.Intn(1000000000000)))
-			b.Error(err)
-			b.Fail()
-		}
+func randomBytes(n int) []byte {
+	bytes := make([]byte, n)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		panic(err)
 	}
-	rs.Unregister()
+
+	return bytes
 }
 
-func checkC1(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
-	return true
-}
+func randomString(bytes []byte) string {
+	for i, b := range bytes {
+		bytes[i] = letters[b%64]
+	}
 
-func actionA1(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
-
-	// change t1 field
-	t1 := tuples["t1"].(model.MutableTuple)
-	t1.SetString(ctx, "p3", "somethingnew")
-
-	firedMap := ruleCtx.(map[string]string)
-	firedMap["A1"] = "Fired"
-}
-
-func checkC2(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
-	return true
-}
-
-func actionA2(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
-	t1 := tuples["t1"]
-	val, _ := t1.GetString("p3")
-	fmt.Println("In Action A2 End ", val)
-	firedMap := ruleCtx.(map[string]string)
-	firedMap["A2"] = "Fired"
-}
-
-func checkC3(ruleName string, condName string, tuples map[model.TupleType]model.Tuple, ctx model.RuleContext) bool {
-	return true
-}
-
-func actionA3(ctx context.Context, rs model.RuleSession, ruleName string, tuples map[model.TupleType]model.Tuple, ruleCtx model.RuleContext) {
-	t1 := tuples["t1"]
-	val, _ := t1.GetString("p3")
-	fmt.Println("In Action A3 End ", val)
-	firedMap := ruleCtx.(map[string]string)
-	firedMap["A3"] = "Fired"
+	return string(bytes)
 }
